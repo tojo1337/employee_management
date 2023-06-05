cb73eb8a52d4a8df0e6e6d53f63f5e2070d260e5
tojo1337 <tojo.mukherjee007@gmail.com>
2023-06-06 00:30:58 +0530

    First commit

---
+++ src/main/java/com/example/empmanage/EmpmanageApplication.java
@@ -0,0 +1,13 @@
+package com.example.empmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmanageApplication.class, args);
+	}
+
+}
