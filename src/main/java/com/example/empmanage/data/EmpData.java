71b6db1d3a12c21d3e8ee7f5fd5d4cbce89e2c11
tojo1337 <tojo.mukherjee007@gmail.com>
2023-06-07 00:52:46 +0530

    3rd commit

---
+++ src/main/java/com/example/empmanage/data/EmpData.java
@@ -0,0 +1,43 @@
+package com.example.empmanage.data;
+
+public class EmpData {
+    private String name;
+    private int age;
+    private int phoneNumber;
+    private float salary;
+
+    public EmpData() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/com/example/empmanage/mapper/PathMapping.java
+++ src/main/java/com/example/empmanage/mapper/PathMapping.java
@@ -1,16 +1,37 @@
 package com.example.empmanage.mapper;
 
+import com.example.empmanage.data.EmpData;
+import com.example.empmanage.data.EmpDataDTO;
+import com.example.empmanage.repo.EmpDataRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class PathMapping {
+    @Autowired
+    private EmpDataRepo repo;
     @GetMapping("/")
     public String index(){
         return "index";
     }
     @GetMapping("/admin_panel")
-    public String adminPanel(){
+    public String adminPanel(Model model){
+        //To show the working employee list
+        List<EmpDataDTO> list = repo.findAll();
+        model.addAttribute("emplist",list);
+        //To add a new employee as an admin
+        EmpData data = new EmpData();
+        model.addAttribute("empdata",data);
         return  "admin_panel";
+    }
+    @PostMapping("/admin_panel")
+    public String addEmpData(Model model, @ModelAttribute("empdata") EmpData data){
+        //This will deal with the instance where admin adds new employee
+        System.out.println("[*]Name : "+data.getName());
+        return "redirect:/admin_panel";
     }
 }
