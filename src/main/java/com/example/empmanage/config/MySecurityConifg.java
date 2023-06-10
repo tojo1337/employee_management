601b0808a3a8aaa3b448999edbfe3fa62f693fa0
tojo1337 <tojo.mukherjee007@gmail.com>
2023-06-10 16:52:37 +0530

    9th commit

--- src/main/java/com/example/empmanage/config/MySecurityConifg.java
+++ src/main/java/com/example/empmanage/config/MySecurityConifg.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,10 +16,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class MySecurityConifg {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.build();
-    }
-    @Bean
-    public CsrfConfigurer csrfConfigurer(){
-        return new CsrfConfigurer<>();
+        return http.csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(requests->requests.
+                        requestMatchers("/").permitAll().
+                        anyRequest().authenticated()).
+                formLogin(login->login.permitAll()).
+                httpBasic(Customizer.withDefaults()).build();
     }
 }
