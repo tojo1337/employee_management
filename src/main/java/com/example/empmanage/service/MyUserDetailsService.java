17c6a76434b2c25c0eeb94bdb5ef4e617de6322e
tojo1337 <tojo.mukherjee007@gmail.com>
2023-06-12 18:19:12 +0530

    13th commit

--- src/main/java/com/example/empmanage/service/MyUserDetailsService.java
+++ src/main/java/com/example/empmanage/service/MyUserDetailsService.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private MyUserRepo repo;
