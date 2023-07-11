97a6adecd5bbffe10a0859529ce5fa15de762dc5
tojo1337 <tojo.mukherjee007@gmail.com>
2023-07-11 20:26:08 +0530

    36th commit

---
+++ src/main/java/com/example/empmanage/data/ResetDataField.java
@@ -0,0 +1,53 @@
+package com.example.empmanage.data;
+//Add the field checks at the end
+//Do not allow the name to be changed
+public class ResetDataField {
+    private String name;
+    private String password;
+    private int age;
+    private int salary;
+    private String phoneNumber;
+
+    public ResetDataField() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
